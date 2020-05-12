f4e50d1439227b788c72fe90d78d42a9ad0d16c0
Aritra Koley <koley.aritra824@gmail.com>
2020-05-12 20:01:13 +0530

    simplified everything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,18 +15,15 @@ export class AppComponent {
     console.log(this.fileList);
 
     // Actual Headers in the Template (comes from API)
-    let template_headers = {
-      mandatory: ['fa', 'fb', 'fc'],
-      optional: ['fd', 'fe']
-     };
+    const templateHeaders = ['fa', 'fb', 'fc'];
 
     // Calls validation function on each file
-    this.validateAllFiles(template_headers, this.fileList);
+    this.validateAllFiles(templateHeaders, this.fileList);
   }
 
   validateAllFiles(th, fl) {
 
-    for ( let i=0; i<this.fileList.length; i++ ) {
+    for ( let i = 0; i < this.fileList.length; i++ ) {
 
       this.validate(th, this.fileList[i], i)
       .then((isValid) => console.log('FILE ' + i + ' VALIDATED AS: ', isValid))
@@ -35,74 +32,37 @@ export class AppComponent {
     }
   }
 
-  matchHeaders(th, fh: string[]) {
-    if ( fh.length < th.mandatory.length ){
-      return false;
-    }
-    else if ( fh.length > (th.mandatory.length + th.optional.length) ) {
+  matchHeaders(th, fh: string[]): boolean {
+    // Assumes all fields in th are mandatory
+    if ( fh.length < th.length ){
       return false;
     }
     else {
+      // (1) Check if all fields are present
+      for ( let i=0; i<th.length; i++ ) {
 
-      // (1) Check if all mandatory fields are present
-      for ( let i=0; i<th.mandatory.length; i++ ) {
-
-        if ( fh.indexOf(th.mandatory[i]) === -1 ) {
+        if ( fh.indexOf(th[i]) === -1 ) {
           return false;
         }
-      }
 
-      // (2) Check if any unmapped fields are present in fh that are not in th
-      for ( let i=0; i<fh.length; i++ ) {
-
-        if ( th.mandatory.indexOf(fh[i]) === -1 && th.optional.indexOf(fh[i]) === -1) {
-          return false;
-        }
       }
     }
 
     return true;
   }
 
-
   validate(th, f: File, index: number) {
 
     return new Promise((resolve, reject) => {
-      let reader = new FileReader();
+      const reader = new FileReader();
 
       reader.onload = () => {
-        let hm: boolean = false;
-        let rm: boolean = false;
 
-        // (1) Match Headers
         const lines = (reader.result as string).split('\n');
-        const fh = lines[0].split(',').map( (x) => { return x.trim(); } );
+        const fh = lines[0].split(',').map( x => x.trim() );
 
-        hm = this.matchHeaders(th, fh);
-
-        if ( hm === true ) {
-          // (2) Match Length of All Other Rows to Length of Headers
-          rm = true;
-          let li: string[];
-          for (let i=1; i<lines.length; i++) {
-
-            li = lines[i].split(',').map( (x) => { return x.trim(); } );
-
-            if ( li.length !== fh.length ) {
-              rm = false;
-              break;
-            }
-          }
-        }
-
-        const isValid: boolean = ( hm && rm );
-
-        // JUST FOR DEBUGGING--------------------------------------
-        const fn = f.name;
-        console.log( { index, fn, lines, hm, rm, isValid } );
-        // --------------------------------------------------------
-
-        resolve(isValid);
+        // console.log({f, index, lines});
+        resolve(this.matchHeaders(th, fh));
       };
 
       reader.readAsText(f);
